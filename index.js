399a84401e7db70a257e6b19b38b910a9440d6d1
Neha Yadav <78076972+Neha5630@users.noreply.github.com>
2023-06-09 00:32:49 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', function (req, res) {
-  res.send('Hello Neha,welcome to this web page')
+  res.send('Hello Neha,welcome to this web-page')
 })
 
 app.listen(4000, ()=>{
